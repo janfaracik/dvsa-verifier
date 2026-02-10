1896a9a48526ae587aaac1185063bb584694543a
Jan <janf@kainos.com>
2026-02-10 15:34:47 +0000

    Add Verify page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {Route, Router} from "./providers/routing-provider.tsx";
 import SplashScreen from "./pages/splash/splash.tsx";
 import Home from "./pages/home/home.tsx";
+import Verify from "./pages/verify/verify.tsx";
 
 export default function App() {
     return (
@@ -11,6 +12,10 @@ export default function App() {
 
             <Route path="home">
                 <Home />
+            </Route>
+
+            <Route path="verify">
+                <Verify />
             </Route>
         </Router>
     );
---
+++ src/components/logo/start.tsx
@@ -0,0 +1,13 @@
+import type {SVGProps} from "react";
+
+const Start = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        viewBox="0 0 33 40"
+        {...props}
+    >
+        <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
+    </svg>
+)
+export default Start
